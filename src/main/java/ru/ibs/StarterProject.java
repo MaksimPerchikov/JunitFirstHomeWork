4e26a239c687956ee8e0bc1d4007aa3b89d71930
MaksimchikPerchikov <maksim.perch999@mail.ru>
2021-10-30 03:54:25 +0300

    Осталось доделать ТЕСТЫ

--- src/main/java/ru/ibs/StarterProject.java
+++ src/main/java/ru/ibs/StarterProject.java
@@ -5,38 +5,70 @@ import java.util.Arrays;
 
 public class StarterProject {
     public static void main(String[] args) {
+        StarterProject starterProject = new StarterProject();
         for (int i = 0; i < args.length; i++) {
             String arStrFin = args[i];
-            methodByStarter(arStrFin);
+           starterProject.methodByStarter(arStrFin);
         }
         //Вставлять прямые ссылки.
         //  String arStrFin = "M:/projects/txt.txt";
-         // String arStrFin = "/projects/txt.txt";
+        // String arStrFin = "/projects/txt.txt";
         // String arStrFin = "txt.txt";
     }
-    public static void methodByStarter(String str){
-     String line;
+
+    public  void methodByStarter(String str) {
+        StarterProject starterProject = new StarterProject();
+       try{
+           Reader reader = new FileReader(str);
+           starterProject.methodTwo(reader);
+       }catch (IOException e){
+           e.printStackTrace();
+       }
+
+       //Изначальный,работающий код!
+       /* String line;
         try {
             Reader reader = new FileReader(str);
             BufferedReader bufferedReader = new BufferedReader(reader);
             line = bufferedReader.readLine();
-            while (line!= null) {
+            while (line != null) {
                 int i = Integer.parseInt(line);
-                if(i%3 == 0 && i%5 == 0){
+                if (i % 3 == 0 && i % 5 == 0) {
                     System.out.println("FizzBuzz");
-                }
-                else if(i%3 ==0){
+                } else if (i % 3 == 0) {
                     System.out.println("Fizz");
-                }else if(i%5 == 0){
+                } else if (i % 5 == 0) {
                     System.out.println("Buzz");
-                }else  System.out.println(line);
+                } else System.out.println(line);
 
                 line = bufferedReader.readLine();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+*/
 
+    }
 
+    public void methodTwo(Reader reader) {
+        String line;
+        try {
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            line = bufferedReader.readLine();
+            while (line != null) {
+                int i = Integer.parseInt(line);
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                } else if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                } else System.out.println(line);
+
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/ru/ibs/StarterProjectTest.java
+++ src/test/java/ru/ibs/StarterProjectTest.java
@@ -1,2 +1,31 @@
-package ru.ibs;public class StarterProjectTest {
+package ru.ibs;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+
+public class StarterProjectTest {
+   /* private StarterProject starterProject;
+    @Before
+    void init(){
+        starterProject = new StarterProject();
+    }*/
+   @Test
+    public void testMethodByStarter() throws FileNotFoundException {
+       StarterProject starterProject = new StarterProject();
+        Reader reader = new FileReader("txtTest.txt");
+        starterProject.methodTwo(reader);
+    }
+
+    @Test
+    public void testMethodTwo()throws FileNotFoundException{
+       Reader reader = new FileReader("txtTest.txt");
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        
+    }
 }
